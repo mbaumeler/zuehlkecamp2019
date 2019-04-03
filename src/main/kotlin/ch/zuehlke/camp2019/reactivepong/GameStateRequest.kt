7d95f748ea4bafa262431aeca2954fe1f8270b3c
Matthias Baumeler <matthias.baumeler@gmail.com>
2019-04-03 10:23:13 +0200

    Use reactive streams instead of state for slider positions.

---
+++ src/main/kotlin/ch/zuehlke/camp2019/reactivepong/GameStateRequest.kt
@@ -0,0 +1,3 @@
+package ch.zuehlke.camp2019.reactivepong
+
+class GameStateRequest(val id: String, val leftSlider: Point, val rightSlider: Point)
--- src/main/kotlin/ch/zuehlke/camp2019/reactivepong/WebsocketController.kt
+++ src/main/kotlin/ch/zuehlke/camp2019/reactivepong/WebsocketController.kt
@@ -1,5 +1,8 @@
 package ch.zuehlke.camp2019.reactivepong
 
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
+import io.reactivex.subjects.BehaviorSubject
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.simp.SimpMessagingTemplate
@@ -9,28 +12,41 @@ import org.springframework.stereotype.Controller
 @Controller
 class WebsocketController constructor(@Autowired val template: SimpMessagingTemplate) {
 
+    private val leftPlayer = BehaviorSubject.createDefault(Point(0.0, 0.0))
+    private val rightPlayer = BehaviorSubject.createDefault(Point(0.0, 0.0))
+
+    private val bothSliderPositions = Observable.combineLatest<Point, Point, Pair<Point, Point>>(leftPlayer, rightPlayer, BiFunction { left, right -> Pair(left, right) })
+
+    private val requestGameStateStream = BehaviorSubject.create<String>()
+
     private val positionController = PositionController()
-    private var leftPosition = Point(0.0, 0.0)
-    private var rightPosition = Point(0.0, 0.0)
+
+    init {
+        requestGameStateStream
+                .withLatestFrom<Pair<Point, Point>, GameStateRequest>(bothSliderPositions, BiFunction<String, Pair<Point, Point>, GameStateRequest> { id, slidePair -> GameStateRequest(id, slidePair.first, slidePair.second) })
+                .subscribe {
+                    template.convertAndSend("/topic/game/" + it.id, currentGameState(it.leftSlider, it.rightSlider))
+                }
+    }
 
     @MessageMapping("/move/LEFT")
     fun onMoveEventLeft(point: Point) {
-        leftPosition = point
+        leftPlayer.onNext(point)
     }
 
     @MessageMapping("/move/RIGHT")
     fun onMoveEventRight(point: Point) {
-        rightPosition = point
+        rightPlayer.onNext(point)
     }
 
     @MessageMapping("/requestGameState")
     fun onRequestGameState(id: String) {
-        template.convertAndSend("/topic/game/" + id, currentGameState())
+        requestGameStateStream.onNext(id)
     }
 
-    fun currentGameState(): GameState {
+    fun currentGameState(left: Point, right: Point): GameState {
         positionController.updatePosition()
         val (xPos, yPos) = positionController.position
-        return GameState(Ball(xPos, yPos), leftPosition, rightPosition)
+        return GameState(Ball(xPos, yPos), left, right)
     }
 }
